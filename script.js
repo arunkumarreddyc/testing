71a38245a398a1146a09002e708da4f5104698c3
arun <arunkumarreddyc2@gmail.com>
2022-01-15 22:07:37 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let para = document.getElementsByTagName("p");
-console.log(para);
-
-function myfunc(){
-for(let i = 0; i < para.length; i++){
-  para[i].style.color = "blue";
-}}
+let display = document.querySelector("#result");
+let buttons = document.querySelectorAll("td");
+let displayValue = "";
+for (but of buttons) {
+  but.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log(buttonText);
+    if (buttonText == "x") {
+      buttonText = "*";
+      displayValue += buttonText;
+      display.value = displayValue;
+    } else if (buttonText == "DEL") {
+      displayValue = displayValue.substring(0, displayValue.length - 1);
+      display.value = displayValue;
+    } else if (buttonText == "%") {
+      displayValue = 1 / 100;
+      display.value = displayValue;
+    } else if (buttonText == "C") {
+      displayValue = "";
+      display.value = displayValue;
+    } else if (buttonText == "=") {
+      display.value = eval(displayValue);
+    } else {
+      displayValue += buttonText;
+      display.value = displayValue;
+    }
+  });
+}
